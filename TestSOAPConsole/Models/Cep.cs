d70edc4b771a46f3f606e65154cd49b9bbd12074
randerthe <randerlealt@gmail.com>
2022-06-29 16:49:06 +0100

    first commit

---
+++ TestSOAPConsole/Models/Cep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TestSOAPConsole.Models
+{
+    public class Cep
+    {
+        [Required]
+        [MaxLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        [MinLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        public string Codigo { get; set; }
+    }
+}
---
+++ TestSOAPConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using TestSOAPConsole.Models;
+
+namespace TestSOAPConsole
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Cep cep = new Cep();
+            Console.WriteLine("Digite o CEP :");
+            cep.Codigo = Console.ReadLine();
+
+            Endereco enderecoEncontrado = GetEndereco(cep);
+
+            Console.WriteLine("Endereço encontrado:");
+            Console.WriteLine(enderecoEncontrado.Descricao);
+            Console.WriteLine(enderecoEncontrado.Complemento);
+            Console.WriteLine(enderecoEncontrado.Bairro);
+            Console.WriteLine(enderecoEncontrado.Cidade);
+            Console.WriteLine(enderecoEncontrado.UF);
+        }
+
+        private static Endereco GetEndereco(Cep cep)
+        {
+            Endereco enderecoEncontrado = new Endereco();
+            var correios = new Correios.AtendeClienteClient();
+
+            var consulta = correios.consultaCEPAsync(cep.Codigo.Replace("-", "")).Result;
+
+            if (consulta != null)
+            {
+                enderecoEncontrado = new Models.Endereco()
+                {
+                    Descricao = consulta.@return.end,
+                    Complemento = consulta.@return.complemento2,
+                    Bairro = consulta.@return.bairro,
+                    Cidade = consulta.@return.cidade,
+                    UF = consulta.@return.uf
+                };
+            }
+            return enderecoEncontrado;
+        }
+    }
+}
